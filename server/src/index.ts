008520c0b56919871c1737d678ddaf2ccf72061d
patiabhishek123 <abhishekcloud2003@gmail.com>
2026-04-02 00:04:18 +0530

    react skelleton

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,15 +4,15 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+
 app.use(express.json());
 
-// Routes
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
 
-// Start server
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
