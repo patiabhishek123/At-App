6352ef79a5d26cdcd8656f9335933a0f4a8c8bab
patiabhishek123 <abhishekcloud2003@gmail.com>
2026-04-03 23:36:40 +0530

    app config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,17 @@
 
-import express from 'express';
+import { app } from "./app"
 
-const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.json());
 
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+    res.json({ message: 'Hello World!' });
 });
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
