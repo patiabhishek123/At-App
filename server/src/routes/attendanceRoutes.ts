86b1fd199238cc155042aa23388d15696d2408ba
patiabhishek123 <abhishekcloud2003@gmail.com>
2026-08-01 22:36:59 +0530

    project refactor

--- server/src/routes/attendanceRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { markAttendanceController } from '../controllers/attendanceController';
-import { authMiddleware } from '../middleware/authMiddleware';
-import { authorizeRoles } from '../middleware/roleMiddleware';
-
-export const attendanceRouter = Router();
-
-attendanceRouter.post('/mark', authMiddleware, authorizeRoles('student'), markAttendanceController);
